092f845e56fed3fbfafe670d6dab093882d63ce6
Sakasasbeh <sami.kasasbehdev@gmail.com>
2024-11-09 21:21:10 -0400

    test

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,8 @@ export default withSentryConfig(withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "javascript-mastery",
-project: "care-pulse",
+org: "sami-kasasbeh",
+project: "javascript-nextjs",
 
 // Only print logs for uploading source maps in CI
 silent: !process.env.CI,
