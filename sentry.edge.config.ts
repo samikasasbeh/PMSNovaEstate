a187831867d99ea9064d1a66ed9869c6b05737ef
Sakasasbeh <sami.kasasbehdev@gmail.com>
2024-11-09 21:14:12 -0400

    test

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,9 +6,9 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://3d627de24f5d06a1fc39000a06ca9a94@o4506813739368448.ingest.us.sentry.io/4507458386526208",
+  dsn: "https://9ebe7b1ad42a2ffa803b829666431580@o4508260809375744.ingest.de.sentry.io/4508270702166096",
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
